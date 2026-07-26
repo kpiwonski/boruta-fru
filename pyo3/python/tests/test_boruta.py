f93fd820adc1b9f947c26cb8d1eea926b20946f3
Chris Piwonski <git@m.kpiwonski.com>
2026-07-26 02:20:53 +0200

    ruff + rust-toolchain

--- pyo3/python/tests/test_boruta.py
+++ pyo3/python/tests/test_boruta.py
@@ -1,7 +1,8 @@
+import boruta_fru
 import numpy as np
 import pandas as pd
-import boruta_fru
 import pytest
+
 
 @pytest.fixture
 def rng():
@@ -20,6 +21,7 @@ def table_0_1_10ft(rng):
     y = pd.Series(y)
     return (df, y)
 
+
 @pytest.fixture
 def table_0_1_10ft_with_nan(rng):
     # Generate random 0s and 1s
@@ -27,11 +29,12 @@ def table_0_1_10ft_with_nan(rng):
     num_cols = 10
     data = {f"col{i + 1}": rng.integers(0, 2, size=num_rows) for i in range(num_cols)}
     df = pd.DataFrame(data)
-    df.iloc[0,1] = np.nan
+    df.iloc[0, 1] = np.nan
     y = pd.Categorical(df.iloc[:, 0], categories=[0, 1], ordered=False)
     y = y.rename_categories({0: "No", 1: "Yes"})
     y = pd.Series(y)
     return (df, y)
+
 
 def test_boruta_x0(table_0_1_10ft):
     b = boruta_fru.Boruta(tries=3, seed=0)
@@ -64,11 +67,11 @@ def test_rf_cls_0_1_3ft_imp_pycapsule(table_0_1_10ft):
     res = b.final_decision(to_pycapsule=True)
     res = pd.DataFrame.from_arrow(res)
     assert list(res.columns) == ["column", "decision"]
-    assert list(res["column"]) == list(["col" + str(i + 1) for i in range(10)])
+    assert list(res["column"]) == ["col" + str(i + 1) for i in range(10)]
     assert res["decision"][0] == "Confirmed"
     assert all(np.isin(b.final_decision()[1:], ["Rejected", "Tentative"]))
 
-    
+
 def test_boruta_x0_with_nan(table_0_1_10ft_with_nan):
     b = boruta_fru.Boruta(tries=3, seed=0)
     b.fit(table_0_1_10ft_with_nan[0], table_0_1_10ft_with_nan[1])
